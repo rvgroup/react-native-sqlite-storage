20159791361a4dc455e618cd43737627bdc9eeca
rvgroup <rudenkovitalij@gmail.com>
2026-05-27 07:45:39 +0300

    +configuration for Google Play where you need to have the native 16kb image
    +creation of Google memory images as required by Google Play Console
    
    from https://github.com/thiagomachado1998/react-native-sqlite-storage

--- platforms/android/android-sqlite-native-driver/jni/Android.mk
+++ platforms/android/android-sqlite-native-driver/jni/Android.mk
@@ -33,9 +33,18 @@ LOCAL_CFLAGS += -DSQLITE_ENABLE_JSON1
 # new stable default page size ref:
 # - http://sqlite.org/releaselog/3_12_0.html
 # - http://sqlite.org/pgszchng2016.html
-LOCAL_CFLAGS += -DSQLITE_DEFAULT_PAGE_SIZE=4096
+LOCAL_CFLAGS += -DSQLITE_DEFAULT_PAGE_SIZE=16384
 LOCAL_CFLAGS += -DSQLITE_DEFAULT_CACHE_SIZE=-2000
 
 LOCAL_SRC_FILES := ../native/sqlc_all.c
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=0x4000
+
+#command used to recompile images
+# /Users/thiagojesus/Library/Android/sdk/ndk/27.1.12297006/ndk-build clean
+# /Users/thiagojesus/Library/Android/sdk/ndk/27.1.12297006/ndk-build \
+# NDK_PROJECT_PATH=. \
+# APP_BUILD_SCRIPT=Android.mk \
+# APP_PLATFORM=android-33 \
+# APP_ABI="armeabi-v7a arm64-v8a x86 x86_64" V=1
 
 include $(BUILD_SHARED_LIBRARY)
